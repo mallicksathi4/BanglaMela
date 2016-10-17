2e0597a8bd4f3311cf411b23f4250fd9a9012bce
Sathi Mallick <mallicksathi4@gmail.com>
2016-10-17 13:26:47 -0400

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,23 @@
+// Example 26-14: javascript.js
+
+canvas               = O('logo')
+context              = canvas.getContext('2d')
+context.font         = 'bold italic 97px Georgia'
+context.textBaseline = 'top'
+image                = new Image()
+image.src            = 'banglamela1.jpg'
+
+image.onload = function()
+{
+  gradient = context.createLinearGradient(0, 0, 0, 89)
+  gradient.addColorStop(0.00, '#faa')
+  gradient.addColorStop(0.66, '#f00')
+  context.fillStyle = gradient
+  context.fillText(  "B  ANGLAMELA", 0, 0)
+  context.strokeText("B  ANGLAMELA", 0, 0)
+  context.drawImage(image, -50, 5)
+}
+
+function O(i) { return typeof i == 'object' ? i : document.getElementById(i) }
+function S(i) { return O(i).style                                            }
+function C(i) { return document.getElementsByClassName(i)                    }
